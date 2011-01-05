943192fe543cb607a20f2b3e4de5ee479ce3966a
Samuel Goodwin <goodwinlabs@gmail.com>
2011-01-04 22:42:39 -0700

    Fixed upload script.

--- tests/zip_sign_upload.rb
+++ tests/zip_sign_upload.rb
@@ -47,18 +47,21 @@ end
 puts "\n"
 
 baseName = filePath.split('/').last
-
+basePath = filePath.gsub(baseName, '')
 zipFileName = baseName.gsub('.app', "#{versionString}.zip")
-if(File.exists?(zipFileName))
-  File.delete(zipFileName)
+zipFilePath = "#{basePath}#{zipFileName}"
+puts "Using zip file Path: #{zipFilePath}"
+if(File.exists?(zipFilePath))
+  puts "File exists, deleting"
+  File.delete(zipFilePath)
 end
 puts "Generating Zip File Called: #{zipFileName}"
-output = IO.popen("zip -r ./#{zipFileName} #{filePath}")
+output = IO.popen("cd #{basePath};zip -r ./#{zipFileName} #{baseName}")
 puts output.readlines
 
 puts "Signing zip file..."
 
-output2 = IO.popen("openssl dgst -sha1 -binary < '#{zipFileName}' | openssl dgst -dss1 -sign '#{key_path}' | openssl enc -base64")
+output2 = IO.popen("openssl dgst -sha1 -binary < '#{zipFilePath}' | openssl dgst -dss1 -sign '#{key_path}' | openssl enc -base64")
 dsaSignature = output2.readlines[0]
 
 puts "Submitting #{zipFileName} with dsaSignature: #{dsaSignature}"
@@ -66,11 +69,12 @@ AWS::S3::Base.establish_connection!(
     :access_key_id     => ENV['AMAZON_ACCESS_KEY_ID'], 
     :secret_access_key => ENV['AMAZON_SECRET_ACCESS_KEY']
 )
-AWS::S3::S3Object.store(zipFileName, open(zipFileName), bucketName)
-
+puts "Looking for file to upload here: #{zipFilePath}"
+AWS::S3::S3Object.store(zipFileName, open("#{zipFilePath}"), bucketName)
+exit
 amazon_url = "https://s3.amazonaws.com/#{bucketName}/#{zipFileName}"
 headers = {:key=>key_string}
-dict = {'buildNumber'=>bundleVersion, 'versionString'=>versionString, 'dsaSignature'=>dsaSignature, "fileURL" => amazon_url, "length"=>File.size(zipFileName)}
+dict = {'buildNumber'=>bundleVersion, 'versionString'=>versionString, 'dsaSignature'=>dsaSignature, "fileURL" => amazon_url, "length"=>File.size(zipFilePath)}
 res = RestClient.post("http://appengine.goodwinlabs.com/#{name}/updates.json", dict, headers);
 res.body
 
